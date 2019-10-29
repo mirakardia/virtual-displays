a0e0e7d14eab69acd8851cc0f08b56afb670a1ee
Pinja Mikkonen <pinjamikkonen@gmail.com>
2019-10-29 16:39:44 +0200

    Create prefab for virtual agent, create waypointpatrol -script

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+
+    // Waypoints virtual agent travels
+    public Transform[] waypoints;
+    int m_CurrentWayPointIndex;
+
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    void Update()
+    {
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWayPointIndex = (m_CurrentWayPointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[m_CurrentWayPointIndex].position);
+        }
+    }
+}
