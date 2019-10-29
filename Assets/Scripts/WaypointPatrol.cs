f9997f0911f39be8d5b7768f5377c615b276af9b
Pinja Mikkonen <pinjamikkonen@gmail.com>
2019-10-29 16:56:51 +0200

    Comment waypoint patrol

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -5,19 +5,22 @@ using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
+    // Virtual agent
     public NavMeshAgent navMeshAgent;
 
-    // Waypoints virtual agent travels
+    // Array of coordinates virtual agent travels through
     public Transform[] waypoints;
     int m_CurrentWayPointIndex;
 
     void Start()
     {
+        // Start at first waypoint: WIP
         navMeshAgent.SetDestination(waypoints[0].position);
     }
 
     void Update()
     {
+        // After reaching waypoint go to next one: if at last one go to waypoint index 0
         if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
         {
             m_CurrentWayPointIndex = (m_CurrentWayPointIndex + 1) % waypoints.Length;
