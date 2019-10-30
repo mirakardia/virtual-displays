c4679b1b1ce39eee4ab35950c528194f133738e5
Pinja Mikkonen <pinjamikkonen@gmail.com>
2019-10-30 14:47:30 +0200

    Comment waypoint patrol, change virtual agent walking speed

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -5,7 +5,6 @@ using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
-    // Virtual agent
     public NavMeshAgent navMeshAgent;
 
     // Array of coordinates virtual agent travels through
@@ -14,35 +13,33 @@ public class WaypointPatrol : MonoBehaviour
     Animator animator;
 
     int m_CurrentWayPointIndex;
-    Vector3 startingPoint;
-    float waitTime;
+    float waitTime;     // How long virtual agent waits at a waypoint
 
     void Start()
     {
         animator = GetComponent<Animator>();
 
-        // Start at first waypoint
         navMeshAgent.SetDestination(waypoints[0].position);
         animator.SetBool("IsWalking", true);
-        waitTime = Random.Range(3.0f, 7.0f);
+        waitTime = Random.Range(4.0f, 8.0f);
     }
 
     void Update()
     {
-        // After reaching waypoint go to next one: if at last one go to waypoint index 0
-        // NOTE: Make sure virtual agent can go from last index to first or change the function
-
+        // After reaching a waypoint wait for waitTime, then move to next waypoint 
+        // If at last waypoint then virtual agent goes to the first waypoint
         if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
         {
             animator.SetBool("IsWalking", false);
-
             waitTime -= Time.deltaTime;
+
             if (waitTime <= 0)
             {
                 animator.SetBool("IsWalking", true);
                 m_CurrentWayPointIndex = (m_CurrentWayPointIndex + 1) % waypoints.Length;
+
                 navMeshAgent.SetDestination(waypoints[m_CurrentWayPointIndex].position);
-                waitTime = Random.Range(3.0f, 7.0f);
+                waitTime = Random.Range(4.0f, 8.0f);
             }
         }
     }
